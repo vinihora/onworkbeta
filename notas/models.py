82dd84a9b78f08643e9669592e5aaedd2a42bdc1
Vinicius Hora <vinidahora08@hotmail.com>
2022-07-07 16:47:30 -0300

    modifing notes

--- notas/models.py
+++ notas/models.py
@@ -2,5 +2,5 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
+    question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField("date published")
