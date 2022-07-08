12a6de09ebf022e17ab7fe0f363cb9313cc83323
Vinicius Hora <vinidahora08@hotmail.com>
2022-07-07 23:04:25 -0300

    QA commit

--- notas/models.py
+++ notas/models.py
@@ -8,3 +8,7 @@ class Question(models.Model):
 class Tes(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField("date published")
+
+class Tes2(models.Model):
+    question_text2 = models.CharField(max_length=250)
+    pub_date = models.DateTimeField("date published")
