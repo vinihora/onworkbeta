10b3b9ccfa44f104e66617cec4c678002d74547a
Vinicius Hora <vinidahora08@hotmail.com>
2022-07-07 22:59:13 -0300

    tes

--- notas/models.py
+++ notas/models.py
@@ -4,3 +4,7 @@ from django.db import models
 class Question(models.Model):
     question_text = models.CharField(max_length=250)
     pub_date = models.DateTimeField("date published")
+
+class Tes(models.Model):
+    question_text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField("date published")
